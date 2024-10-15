bb61e27edf85375b0c0d72bbea7d8970ba9b986b
wangbinDL <wangbin_research@163.com>
2024-10-15 10:23:46 +0800

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,5 +1,6 @@
 import os
 import cv2
+import torch
 import argparse
 from doclayout_yolo import YOLOv10
 
@@ -8,20 +9,28 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--model', default=None, required=True, type=str)
     parser.add_argument('--image-path', default=None, required=True, type=str)
-    parser.add_argument('--device', default=None, required=True, type=str)
+    parser.add_argument('--res-path', default='outputs', required=False, type=str)
     parser.add_argument('--imgsz', default=1024, required=False, type=int)
     parser.add_argument('--line-width', default=5, required=False, type=int)
     parser.add_argument('--font-size', default=20, required=False, type=int)
     parser.add_argument('--conf', default=0.2, required=False, type=float)
     args = parser.parse_args()
     
+    # Automatically select device
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    print(f"Using device: {device}")
+
     model = YOLOv10(args.model)  # load an official model
 
     det_res = model.predict(
-        args.image_file,
+        args.image_path,
         imgsz=args.imgsz,
         conf=args.conf,
-        device=args.device,
+        device=device,
     )
     annotated_frame = det_res[0].plot(pil=True, line_width=args.line_width, font_size=args.font_size)
-    cv2.imwrite(args.image_path.split("/")[-1].replace(".png", "_res.png"), annotated_frame)
+    if not os.path.exists(args.res_path):
+        os.makedirs(args.res_path)
+    output_path = os.path.join(args.res_path, args.image_path.split("/")[-1].replace(".png", "_res.png"))
+    cv2.imwrite(output_path, annotated_frame)
+    print(f"Result saved to {output_path}")
